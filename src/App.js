a07d3d55d74b3bd90233cb17fab864e572d46463
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-18 13:37:13 +0200

    Lagt in två komponenter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Search from './Search';
+import Results from './Results';
 
 function App() {
   return (
     <div>
       <h1>Hitta låten</h1>
+      <Search/>
+      <Results/>
     </div>
   );
 }
