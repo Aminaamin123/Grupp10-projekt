5760d3b44bc13807b3e64c1b8b11795cd8a2173b
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-20 14:38:20 +0200

    API Spotify authorization & request

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,47 +1,58 @@
-import React, { useState, useEffect, ReactNode } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 
 export default function Spotify() {
-    // Client Credentials 
-    const accessToken = ""
-    const tokenURL = "https://accounts.spotify.com/api/token"
-    // Client Values
-    const clientId = "b936ad39fb5d455d9feaf0d6e67b51cd"
-    const clientSecret = "145d66c67c274bfd867441f32a30354e"
-    const grantType = "client_credentials"
-    const headers = {"Authorization": "Basic " + clientId + ":" + clientSecret}
-    const data = {"grant_type": "client_credentials"}
-
-    function authorize(){
-        console.log("hello")
-        axios.get(tokenURL).then(response =>{
-            console.log(response.data)
-        }, [tokenURL])
-    }
-    authorize()
-
-    // test url
-    const url = "http://www.omdbapi.com/?apikey=7c75e03b&i=tt1285016"
     const [song, setSong] = useState(null)
 
     useEffect(() => {
-        axios.get(url).then(response =>{
-            setSong(response.data)
-        })
+        var URL = 'https://api.spotify.com/v1/tracks/6rqhFgbbKwnb9MLmUQDhG6'
+        var request = require('request');
+        var client_id = 'b936ad39fb5d455d9feaf0d6e67b51cd'; // client id
+        var client_secret = '145d66c67c274bfd867441f32a30354e'; //  secret
+        
+        // requests authorization
+        var authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+          },
+          form: {
+            grant_type: 'client_credentials'
+          },
+          json: true
+        };
+        
+        // request the song from spotify by id
+        request.post(authOptions, function(error, response, body) {
+          if (!error && response.statusCode === 200) {
+        
+            var token = body.access_token;
+            var options = {
+              url: URL,
+              headers: {
+                'Authorization': 'Bearer ' + token
+              },
+              json: true
+            };
+            request.get(options, function(error, response, body) {
+              console.log(body);
+              setSong(body.external_urls.spotify)
+            });
+          }
+        });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [url])
+    }, [URL])
 
     if(song){
         return (
             <div>
-                Spotify {song.Title}
+                Spotify {song}
             </div>
         )
     }
     return(
         <div>
-            
+            Spotify did not find song
         </div>
     )
 }
