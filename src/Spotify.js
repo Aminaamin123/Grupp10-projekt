058f1574f2049655c890f696ea9c0ad7ae8f7089
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-21 23:36:20 +0200

    Brutit ut variabler & lagt till kommentarer

--- src/Spotify.js
+++ src/Spotify.js
@@ -5,11 +5,13 @@ export default function Spotify() {
     const [song, setSong] = useState(null)
 
     useEffect(() => {
-        var URL = 'https://api.spotify.com/v1/tracks/6rqhFgbbKwnb9MLmUQDhG6'
+        var URL = 'https://api.spotify.com/v1/tracks/' // link of API
+        // 6rqhFgbbKwnb9MLmUQDhG6 (should be the id from Song-component) 
+        URL += "6rqhFgbbKwnb9MLmUQDhG6" // adding track id to link
         var request = require('request');
+        // client credentials 
         var client_id = 'b936ad39fb5d455d9feaf0d6e67b51cd'; // client id
         var client_secret = '145d66c67c274bfd867441f32a30354e'; //  secret
-        
         // requests authorization
         var authOptions = {
           url: 'https://accounts.spotify.com/api/token',
@@ -21,41 +23,39 @@ export default function Spotify() {
           },
           json: true
         };
-        
         // request the song from spotify by id
         request.post(authOptions, function(error, response, body) {
           if (!error && response.statusCode === 200) {
-        
-            var token = body.access_token;
+            var token = body.access_token; 
             var options = {
               url: URL,
               headers: {
-                'Authorization': 'Bearer ' + token
+                'Authorization': 'Bearer ' + token 
               },
               json: true
             };
             request.get(options, function(error, response, body) {
-              console.log(body);
+              console.log(body); // REMOVE, test printing
               setSong(body)
             });
           }
         });
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [URL])
-     //"spotify:album:3a0UOgDWw2pTajw85QPMiz"
+
     if(song){
+      var link = "https://open.spotify.com/embed/album/"
+      link += song.album.id //example 3a0UOgDWw2pTajw85QPMiz
         return (
             <div>
                 Spotify {song.external_urls.spotify} {song.track_number} {song.name} {song.uri} {song.album.id} 
-                <iframe src="https://open.spotify.com/embed/album/3a0UOgDWw2pTajw85QPMiz" width="500" height="380" allowtransparency="true" allow="encrypted-media"></iframe>
-
-                <iframe src="https://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6" width="500" height="380" allowtransparency="true" allow="encrypted-media"></iframe>
+                <iframe src={link} width="500" height="380" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
         )
     }
     return(
         <div>
-            Spotify did not find song
+            Spotify - did not find song
         </div>
     )
 }
