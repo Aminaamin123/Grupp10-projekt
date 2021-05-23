9250dfc818bd80838d68780e4ff81b139f084e0b
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-23 15:34:38 +0200

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Results from './Results';
 
 export default function Search() {
+    const lyric = ""
 
+    const inputLyric = useRef();
+
+    function setLyric(){
+       lyric = inputLyric.current.value
+    }
     // TODO get a string to send to Resultas
-    const search = "lyric"
+
     return (
         <div>
-            Search:.... 
-            Get resultat songs by using lyric as input
-            <Results item={search}/>
+            <from>
+                <fieldset>
+                    <div className="mb-3">
+                        <label for="lyric">Search for after a song:</label>
+                        <input className="form-control" type="text" id="lyric" placeholder="Enter your lyric..." ref={inputLyric} />
+                    </div>
+                    <input type="submit" className="btn btn-success mt-3" value="Search" onClick={setLyric} />
+                </fieldset>
+            </from>
+            <Results item={lyric}/>
         </div>
     )
 }
