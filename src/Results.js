829a4126638ed62a6ebc63a63973d21e72288767
Jakob Persson <jakob.e.persson@gmail.com>
2021-05-26 15:08:24 +0200

    Hämtar från API och sätter Song till response

--- src/Results.js
+++ src/Results.js
@@ -9,28 +9,37 @@ export default function Results(props) {
     console.log(props.item)
     //const songs = []
     const [songs, setSongs] = useState([]);
-    const proxy = "https://cors-anywhere.herokuapp.com/"
-    const url = "https://api.musixmatch.com/ws/1.1/track.get?commontrack_id=11278&apikey=e9882bc5eb026434a2d1fadbecb10d5a"
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+
+    const searchWord = "looking california but feeling minnesota";
+
+    const urlGetLyrics = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?commontrack_id=11278&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
+    const urlSearchLyrics = "https://api.musixmatch.com/ws/1.1/track.search?q_lyrics="+ searchWord + "&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
+    const urlSearchTrack = "https://api.musixmatch.com/ws/1.1/track.search?q_artist=nirvana&page_size=3&page=1&s_track_rating=desc&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
     const axios = require('axios');
+
 
     useEffect(() => {
         // GET request using axios inside useEffect React hook
-        axios.get(proxy+url)
+        axios.get(proxy+urlSearchLyrics)
             .then(function (response) {
               console.log(response.data)
-              setSongs(response.data)
+              setSongs(response.data.message.body.track_list)
+              
             });
     
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    }, [url]);
-    
+    }, [urlSearchLyrics]);
         console.log(songs);
+        //console.log(songs.message.body.lyrics.lyrics_id);
+        //console.log(songs.message.body.track_list[0].track.track_name)
+    
 
     if (songs > 1){
         return (
             //TODO API request to match song, Show resultat of all matching songs
             <div>
-               
+                {songs}
                 If only one song: display on spotify else: list + click function
                 <ul>
                     All matches <Song/> 
@@ -40,6 +49,9 @@ export default function Results(props) {
         )
     }
     return (
+        <div>
+        
         <Spotify item={songs}/> 
+        </div>
     )
 }
