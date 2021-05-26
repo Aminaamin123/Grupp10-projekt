e6fc9b93882c2206abb6fbe7cd8825735038c112
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-26 15:42:22 +0200

    bakgrund

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Search from './Search';
 
 function App() {
   return (
-    <div className="">
+    <div class="container">
       <h1 className="display-4 fw-normal" >Correspondre</h1>
       <p> Welcome, here you can search for song lyric and get songs that includes the lyric previewed & and a direct link to the album on spotify.  </p>
       <Search/>
