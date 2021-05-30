d45d3359a3a251f91fbdc107095ffa26e8f8eb37
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-30 11:36:28 +0200

    la till react-modal - tog bort komponent modal

--- src/Results.js
+++ src/Results.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import Spotify from './Spotify';
+import React, {useState, useEffect} from 'react';
 import Song from './Song';
-
-import {useState, useEffect} from 'react';
+import Modal from './Modal-test';
 import axios from 'axios';
 
 export default function Results(props) {
-    console.log(props.item)
-    //const songs = []
     const [songs, setSongs] = useState([]);
     const proxy = "https://cors-anywhere.herokuapp.com/";
 
-    const searchWord = "looking california but feeling minnesota";
-
-    const urlGetLyrics = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?commontrack_id=11278&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
+    //const urlGetLyrics = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?commontrack_id=11278&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
     const urlSearchLyrics = "https://api.musixmatch.com/ws/1.1/track.search?q_lyrics="+ props.item + "&page_size=50&s_track_rating=desc&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
-    const urlSearchTrack = "https://api.musixmatch.com/ws/1.1/track.search?q_artist=nirvana&page_size=3&page=1&s_track_rating=desc&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
+    //const urlSearchTrack = "https://api.musixmatch.com/ws/1.1/track.search?q_artist=nirvana&page_size=3&page=1&s_track_rating=desc&apikey=e9882bc5eb026434a2d1fadbecb10d5a";
     const axios = require('axios');
 
     useEffect(() => {
@@ -24,7 +18,6 @@ export default function Results(props) {
             .then(function (response) {
               console.log(response.data)
               setSongs(response.data.message.body.track_list)
-              setSongs(songs.filter((item) => item.track.track_rating > 5));
             });
     
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
@@ -34,21 +27,19 @@ export default function Results(props) {
         //console.log(songs.message.body.track_list[0].track.track_name)
         //{songs.map(song => <Song key={song.track.track_id} item={song} />) }
            
+        if (songs.length > 1){
+            return (
+                <div>
+                    <ul>
+                        {songs.map(song => <Song key={song.track.track_id} item={song} />) }
+                    </ul>
+                </div>
+            )
+        }
 
-    if (songs.length > 1){
-        return (
-            //TODO API request to match song, Show resultat of all matching songs
-            <div>
-                
-                If only one song: display on spotify else: list + click function
-                <ul>
-                {songs.map(song => <Song key={song.track.track_id} item={song} />) }
-                </ul>
-            </div>
-        )
-    }
     return (
         <div>
+            TODO - loading icon 
         </div>
     )
 }
--- src/Song.js
+++ src/Song.js
@@ -1,9 +1,37 @@
 import { div } from 'prelude-ls';
 import React from 'react'
 import {useState, useEffect} from 'react';
-import Modal from './Modal';
+import Modal from 'react-modal';
 
 export default function Song(props) {
+
+    const customStyles = {
+        content : {
+          top                   : '50%',
+          left                  : '50%',
+          right                 : 'auto',
+          bottom                : 'auto',
+          marginRight           : '-50%',
+          transform             : 'translate(-50%, -50%)'
+        }
+      };
+    
+      var subtitle;
+      const [modalIsOpen,setIsOpen] = React.useState(false);
+      function openModal() {
+        setIsOpen(true);
+      }
+    
+      function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+      }
+    
+      function closeModal(){
+        setIsOpen(false);
+      }
+
+
     const [info, setInfo] = useState([]);
 
     const proxy = "https://cors-anywhere.herokuapp.com/";
@@ -17,15 +45,34 @@ export default function Song(props) {
                 .then(function (response) {
                   console.log(response.data)
                   alert(response.data.message.body.lyrics.lyrics_body)
-                  
                 });
     }
     return (
 
         <div>
         <li>
-            <Modal item={props.item}/> 
-            <button class="btn btn-success me-3 mb-3" onClick={() => DisplaySong()}> Get full lyric </button>
+            
+        
+      <div>
+        <button className="btn btn-success me-3 mb-3" onClick={openModal}>Preview on Spotify</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+          <button type="button" className="btn btn-secondary" onClick={closeModal}>Close</button>
+          <h1 className="mt-3 text-white bg-success ps-1 pe-4">Spotify</h1>
+          SPOTIFY COMPONENT...
+        </Modal>
+      </div>
+    
+
+            
+            <button className="btn btn-success me-3 mb-3" onClick={() => DisplaySong()}> Get full lyric </button>
             {props.item.track.artist_name} -
             {props.item.track.track_name}
         </li>
@@ -33,3 +80,12 @@ export default function Song(props) {
 
     )
 }
+
+
+
+
+
+
+
+
+
