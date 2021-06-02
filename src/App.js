c6a0152efa7d5db66dd1446a1fd7955833672571
tiffanykh <tiffanyykh12@gmail.com>
2021-06-02 14:00:10 +0200

    Footer fixat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Search from './Search';
 import Footer from './Footer';
+const minHeight="500px";
 
 function App() {
   return (
-    <div class="container">
+    <div>
+      <div class="container" style={{minHeight: minHeight}}>
       <h1 className="display-4 fw-normal" >Correspondre</h1>
       <p> Welcome, here you can search for song lyric and get songs that includes the lyric previewed & and a direct link to the album on spotify.  </p>
       <Search/>
+      </div>
       <Footer/> 
     </div>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
-export default function App() {
+export default function Footer() {
   return (
-    <footer className="bd-footer py-5 mt-5 bg-light">
-        <p>Amina Amin, Tiffany Khayyami och Jakob Persson.</p>
-        <p>Malmö Universitet</p>
-    </footer>
+    <div class="mt-5 pt-4 pb-6 footer bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-xs-12 about-company">
+          <h2>Description</h2>
+          <p class="pr-5 text-black-50">We have created an application where you can get previews and lyrics on songs by searching for song and artist name. You then get an overview of songs that you have recently searched for. The idea behind the project was that all group members love music, so it seemed natural to us that the project would be about some music related. </p>
+          <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+        </div>
+        <div class="col-lg-3 col-xs-12 links">
+          <h4 class="mt-lg-0 mt-sm-3">Group Members</h4>
+            <ul class="m-0 p-0">
+              <li>Amina Amin</li>
+              <li>Tiffany Khayyami</li>
+              <li>Jakob Persson</li>
+            </ul>
+        </div>
+        <div class="col-lg-4 col-xs-12 location">
+          <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+          <p>Malmö Universitet</p>
+          <p>Nordenskiöldsgatan 1, 211 19 Malmö</p>
+          <p><i class="fa fa-envelope-o mr-3"></i>Course: Multi-platform applications with web technician-VT21</p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col copyright">
+          <p class=""><small class="text-black-50">© 2020. All Rights Reserved.</small></p>
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
